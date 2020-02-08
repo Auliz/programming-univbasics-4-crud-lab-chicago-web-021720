4f28b4389d799fca2e32cb4dcc986cbc7c0714e2
Joseph E Auz <joseph.auz77@gmail.com>
2020-02-08 07:47:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  four_array = [druid, mage, hunter, rogue]
+  four_array = [druid, mage, hunter, rogue, paladin]
 end
 
 def add_element_to_end_of_array(array, element)
